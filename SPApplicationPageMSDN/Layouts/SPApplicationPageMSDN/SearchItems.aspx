a8de387f24415dc20c3da649bbf878004701e4ad
Sherif Abdellatif <sharif_pc@hotmail.com>
2017-12-24 10:06:35 +0400

    A Working Sample that counts all lists in the Farm

---
+++ SPApplicationPageMSDN/Layouts/SPApplicationPageMSDN/SearchItems.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Assembly Name="$SharePoint.Project.AssemblyFullName$" %>
+<%@ Import Namespace="Microsoft.SharePoint.ApplicationPages" %>
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SearchItems.aspx.cs" Inherits="SPApplicationPageMSDN.Layouts.SPApplicationPageMSDN.SearchItems" DynamicMasterPageFile="~masterurl/default.master" %>
+
+<asp:Content ID="PageHead" ContentPlaceHolderID="PlaceHolderAdditionalPageHead" runat="server">
+
+</asp:Content>
+
+<asp:Content ID="Main" ContentPlaceHolderID="PlaceHolderMain" runat="server">
+    <asp:Label runat="server" ID="Label1" Text="Show All Items"></asp:Label>
+    <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" >
+        <asp:ListItem Text="Created by me" Value="Author"></asp:ListItem>  
+        <asp:ListItem Text="Modified by me" Value="Editor"></asp:ListItem>  
+    </asp:DropDownList>
+    <asp:Table ID="Table1" runat="server"></asp:Table>
+</asp:Content>
+
+<asp:Content ID="PageTitle" ContentPlaceHolderID="PlaceHolderPageTitle" runat="server">
+SPApplicationPageMSDN
+</asp:Content>
+
+<asp:Content ID="PageTitleInTitleArea" ContentPlaceHolderID="PlaceHolderPageTitleInTitleArea" runat="server" >
+SharePoint Application Page Walkthrough from MSDN
+</asp:Content>
---
+++ SPApplicationPageMSDN/Layouts/SPApplicationPageMSDN/SearchItems.aspx.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.WebControls;
+using System;
+using System.Collections;
+using System.Web.UI.WebControls;
+
+namespace SPApplicationPageMSDN.Layouts.SPApplicationPageMSDN
+{
+    public partial class SearchItems : LayoutsPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SPSecurity.RunWithElevatedPrivileges(GetItems);
+        }
+
+        private void GetItems()
+        {
+            SPFieldUserValue currentUser = new SPFieldUserValue(this.Web, this.Web.CurrentUser.ID, this.Web.CurrentUser.Name);
+
+            ArrayList resultsList = new ArrayList();
+
+            SPFarm thisFarm = SPFarm.Local;
+            SPWebService service = thisFarm.Services.GetValue<SPWebService>("");
+
+            foreach (SPWebApplication webApp in service.WebApplications)
+            {
+                foreach (SPSite siteCollection in webApp.Sites)
+                {
+                    foreach (SPWeb web in siteCollection.AllWebs)
+                    {
+                        SPListCollection lists = web.Lists;
+                        foreach (SPList list in lists)
+                        {
+                            try
+                            {
+                                resultsList.Add(list.Title);
+
+                                #region I removed this because it's a very large query 
+                                //foreach (SPListItem item in list.Items)
+                                //{
+                                //    if (item[DropDownList1.SelectedValue].ToString() == currentUser.ToString())
+                                //    {
+                                //        resultsList.Add(item);
+                                //    }
+                                //} 
+                                #endregion
+                            }
+                            catch (Exception)
+                            {
+                                // An error with the list. Move onto the next list.
+                            }
+                        }
+                    }
+                }
+            }
+
+            Label1.Text = "You have " + resultsList.Count +" Lists in this Farm";
+
+            //CreateResultsTable(resultsList);
+        }
+
+        //private void CreateResultsTable(ArrayList resultsList)
+        //{
+        //    string currentList = "";
+        //    string currentSite = "";
+        //    Table1.Rows.Clear();
+
+        //    foreach (SPListItem item in resultsList)
+        //    {
+        //        if (item.ParentList.ParentWeb.Title != currentSite)
+        //        {
+        //            currentSite = item.ParentList.ParentWeb.Title;
+
+        //            TableCell newSiteCell = new TableCell();
+        //            newSiteCell.Text = currentSite;
+
+        //            TableRow newSiteRow = new TableRow();
+        //            newSiteRow.Cells.Add(newSiteCell);
+        //            newSiteRow.Font.Bold = true;
+        //            newSiteRow.Font.Size = FontUnit.Larger;
+        //            newSiteRow.Font.Underline = true;
+
+        //            Table1.Rows.Add(newSiteRow);
+        //        }
+        //        if (item.ParentList.Title != currentList)
+        //        {
+        //            currentList = item.ParentList.Title;
+        //            TableCell newListCell = new TableCell();
+        //            newListCell.Text = currentList;
+
+        //            TableRow newListRow = new TableRow();
+        //            newListRow.Cells.Add(newListCell);
+        //            newListRow.Font.Bold = true;
+
+        //            Table1.Rows.Add(newListRow);
+
+        //            TableCell itemHeading = new TableCell();
+        //            itemHeading.Text = "Item";
+        //            itemHeading.Font.Italic = true;
+
+        //            TableCell createdHeading = new TableCell();
+        //            createdHeading.Text = "Created";
+        //            createdHeading.Font.Italic = true;
+
+        //            TableCell modifiedHeading = new TableCell();
+        //            modifiedHeading.Text = "Last Modified";
+        //            modifiedHeading.Font.Italic = true;
+
+        //            TableRow headingRow = new TableRow();
+        //            headingRow.Cells.Add(itemHeading);
+        //            headingRow.Cells.Add(createdHeading);
+        //            headingRow.Cells.Add(modifiedHeading);
+
+        //            Table1.Rows.Add(headingRow);
+        //        }
+
+        //        TableCell itemName = new TableCell();
+        //        HyperLink itemLink = new HyperLink();
+        //        try
+        //        {
+        //            itemLink.NavigateUrl = item.ParentList.ParentWeb.Url + "/" +
+        //            item.ParentList.Forms[PAGETYPE.PAGE_DISPLAYFORM].Url + "?ID=" + item.ID;
+        //        }
+        //        catch (Exception)
+        //        {
+        //            // Some items might not have a form page. Ignore the exception.
+        //        }
+        //        itemLink.Text = item.DisplayName;
+        //        itemName.Controls.Add(itemLink);
+
+        //        TableCell created = new TableCell();
+        //        created.Text = item["Created"].ToString();
+
+        //        TableCell modified = new TableCell();
+        //        modified.Text = item["Modified"].ToString();
+
+        //        TableRow dataRow = new TableRow();
+        //        dataRow.Cells.Add(itemName);
+        //        dataRow.Cells.Add(created);
+        //        dataRow.Cells.Add(modified);
+
+        //        Table1.Rows.Add(dataRow);
+        //    }
+        //}
+    }
+}
